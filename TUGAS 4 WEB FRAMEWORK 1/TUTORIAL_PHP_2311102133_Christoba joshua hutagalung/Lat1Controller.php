0a65c977247e81af7d6e2512d13751c790ad6108
joshua <christobahutagalung2005@gmail.com>
2026-03-30 21:38:03 +0700

    Upload tugas 4 web framework 1

--- TUGAS 4 WEB FRAMEWORK 1/TUTORIAL_PHP_2311102133_Christoba joshua hutagalung/Lat1Controller.php
+++ TUGAS 4 WEB FRAMEWORK 1/TUTORIAL_PHP_2311102133_Christoba joshua hutagalung/Lat1Controller.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 
 class Lat1Controller extends Controller{
     public function index() {
-        $data["nama"]="Muhammad Dhimas Hafizh Fathurrahman";
-        $data["NIM"]="2311102151";
+        $data["nama"]="Christoba joshua hutagalung";
+        $data["NIM"]="2311102133";
         $data["Kelas"]="PS1IF-11-REG04";
-        $data["asal"]="Bekasi";
+        $data["asal"]="Samarinda";
         return view('v_latihan1', $data);
     }
 
     public function method2(){
         $data['title'] = "Daftar Mahasiswa";
         $data['daf_mhs'] = array(
-            array("nama" => "M Dhimas Hafizh F", "asal" => "Bekasi"),
+            array("nama" => "Christoba joshua hutagalung", "asal" => "Samarinda"),
             array("nama" => "Agus", "asal" => "Bandung"),
             array("nama" => "Budi", "asal" => "Jakarta"),
             array("nama" => "Roni", "asal" => "Surabaya")
